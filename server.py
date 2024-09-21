7298dbacd560965d80823a1740bac37750200500
Andy <obrezkov.andy@gmail.com>
2024-09-21 14:15:10 +0300

    Improve html

--- server.py
+++ server.py
@@ -16,4 +16,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(port=8082, host='127.0.0.1')
+    app.run(port=8082, host='127.0.0.1', debug=True, use_reloader=True)
